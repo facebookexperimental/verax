serenedb/axiom

Squashed commit of the following:
commit c37a1cfd612bde2b1a0dfea2d42495e2a65b92ad
Author: Orri Erling <oerling@meta.com>
Date:   Mon Sep 1 20:52:26 2025 -0700

    feat: Add table write logical plan node and partition type (#238)

    Summary:
    Adds a logical plan node for table write and generate write plans and produce a write finishing function.

    Adds a PartitionType to ConnectorMetadata. This supports flexible determiniation of possible copartitioning.

    - Uses  connector::PartitionType to describe a partition function.

    - Adds a ConnectorMetadata method for getting the result type of a table write.

    - Adds failure recovery to LocalHiveConnectorMetadata. Files are first staged in a temp directory and moved to the destination only at in the success case of the write finishing function.

    - Adds a separate write execution path to LocalRunner. This silenntly gathers results from the write query and passes these to the write finishing function. Calls the write finishing function with success or with failure if the query has an error.

    Pull Request resolved: https://github.com/facebookexperimental/verax/pull/238

    Differential Revision: D81461538

    Pulled By: oerling

axiom/logical_plan/LogicalPlanNode.h
@@ -33,6 +33,7 @@ enum class NodeKind {
   kLimit = 7,
   kSet = 8,
   kUnnest = 9,
+  kTableWrite = 10,
 };
 
 VELOX_DECLARE_ENUM_NAME(NodeKind)
@@ -653,4 +654,112 @@ class UnnestNode : public LogicalPlanNode {
 
 using UnnestNodePtr = std::shared_ptr<const UnnestNode>;
 
+/// Corresponds to connector::WriteKind.
+enum class WriteKind {
+  // Rows are added . All columns are written and either have a value in from an
+  // expression in the table writer or get a default from the schema. This
+  // covers insert, create table and replacing a Hive partition and any other
+  // use that adds whole rows.
+  kInsert,
+
+  // Individual rows are deleted. Only row ids as per
+  // ConnectorMetadata::rowIdHandles() are passed to the TableWriter.
+  kDelete,
+
+  // Column values in individual rows are changed. The TableWriter
+  // gets first the row ids as per ConnectorMetadata::rowIdHandles()
+  // and then new values for the columns being changed. The new values
+  // may overlap with row ids if the row id is a set of primary key
+  // columns.
+  kUpdate
+};
+
+VELOX_DECLARE_ENUM_NAME(WriteKind);
+
+/// Implements insert/delete/update as per 'kind'.
+class TableWriteNode : public LogicalPlanNode {
+ public:
+  /// @param id Unique ID of the plan node.
+  /// @param connectorId ID of the connector to use to access the table.
+  /// @param tableName Table name.
+  /// @param kind - Indicates the type of write (insert/delete/update)
+  /// @param values - Expressions producing the values to write.. Correspond 1:1
+  /// to 'columnNames'.
+  /// @param columnNames A List of columns in the table being written. 1:1 to
+  /// 'inputNames'. 'columnNames' must refer to columns in the table but their
+  /// number or order does not have to correspond to the table. Missing columns
+  /// in insert get their default from the table.
+  /// @param outputType - Connector dependent output.
+  /// @param options - Writer dependent options. Mayy specify compression or
+  /// encoding options. The table always specifies partitioning. 'options' are
+  /// only for advanced/testing features.
+  TableWriteNode(
+      const std::string& id,
+      const LogicalPlanNodePtr& input,
+      const std::string& connectorId,
+      const std::string& tableName,
+      WriteKind kind,
+      const std::vector<ExprPtr>& values,
+      const std::vector<std::string>& columnNames,
+      const RowTypePtr& outputType,
+      const std::unordered_map<std::string, std::string>& options = {})
+      : LogicalPlanNode(NodeKind::kTableWrite, id, {input}, outputType),
+        connectorId_(connectorId),
+        tableName_(tableName),
+        writeKind_(kind),
+        values_(values),
+        columnNames_(columnNames),
+        options_(options) {}
+
+  const std::string& connectorId() const {
+    return connectorId_;
+  }
+
+  const std::string& tableName() const {
+    return tableName_;
+  }
+
+  WriteKind writeKind() const {
+    return writeKind_;
+  }
+
+  const std::vector<ExprPtr>& values() const {
+    return values_;
+  }
+
+  const std::vector<std::string>& columnNames() const {
+    return columnNames_;
+  }
+
+  const std::unordered_map<std::string, std::string>& options() const {
+    return options_;
+  }
+
+  void accept(const PlanNodeVisitor& visitor, PlanNodeVisitorContext& context)
+      const override;
+
+ private:
+  static RowTypePtr makeWriteType();
+
+  const std::string connectorId_;
+  const std::string tableName_;
+  const WriteKind writeKind_;
+  const std::vector<ExprPtr> values_;
+  const std::vector<std::string> columnNames_;
+  const std::unordered_map<std::string, std::string> options_;
+};
+
+using TableWriteNodePtr = std::shared_ptr<const TableWriteNode>;
+
 } // namespace facebook::velox::logical_plan
+
+template <>
+struct fmt::formatter<facebook::velox::logical_plan::WriteKind>
+    : fmt::formatter<string_view> {
+  template <typename FormatContext>
+  auto format(facebook::velox::logical_plan::WriteKind k, FormatContext& ctx)
+      const {
+    return formatter<string_view>::format(
+        facebook::velox::logical_plan::WriteKindName::toName(k), ctx);
+  }
+};

facebookexperimental/verax

axiom/logical_plan/LogicalPlanNode.h
@@ -33,6 +33,7 @@ enum class NodeKind {
   kLimit = 7,
   kSet = 8,
   kUnnest = 9,
+  kTableWrite = 10,
 };
 
 VELOX_DECLARE_ENUM_NAME(NodeKind)
@@ -656,4 +657,112 @@ class UnnestNode : public LogicalPlanNode {
 
 using UnnestNodePtr = std::shared_ptr<const UnnestNode>;
 
+/// Corresponds to connector::WriteKind.
+enum class WriteKind {
+  // Rows are added . All columns are written and either have a value in from an
+  // expression in the table writer or get a default from the schema. This
+  // covers insert, create table and replacing a Hive partition and any other
+  // use that adds whole rows.
+  kInsert,
+
+  // Individual rows are deleted. Only row ids as per
+  // ConnectorMetadata::rowIdHandles() are passed to the TableWriter.
+  kDelete,
+
+  // Column values in individual rows are changed. The TableWriter
+  // gets first the row ids as per ConnectorMetadata::rowIdHandles()
+  // and then new values for the columns being changed. The new values
+  // may overlap with row ids if the row id is a set of primary key
+  // columns.
+  kUpdate
+};
+
+VELOX_DECLARE_ENUM_NAME(WriteKind);
+
+/// Implements insert/delete/update as per 'kind'.
+class TableWriteNode : public LogicalPlanNode {
+ public:
+  /// @param id Unique ID of the plan node.
+  /// @param connectorId ID of the connector to use to access the table.
+  /// @param tableName Table name.
+  /// @param kind - Indicates the type of write (insert/delete/update)
+  /// @param values - Expressions producing the values to write.. Correspond 1:1
+  /// to 'columnNames'.
+  /// @param columnNames A List of columns in the table being written. 1:1 to
+  /// 'inputNames'. 'columnNames' must refer to columns in the table but their
+  /// number or order does not have to correspond to the table. Missing columns
+  /// in insert get their default from the table.
+  /// @param outputType - Connector dependent output.
+  /// @param options - Writer dependent options. Mayy specify compression or
+  /// encoding options. The table always specifies partitioning. 'options' are
+  /// only for advanced/testing features.
+  TableWriteNode(
+      const std::string& id,
+      const LogicalPlanNodePtr& input,
+      const std::string& connectorId,
+      const std::string& tableName,
+      WriteKind kind,
+      const std::vector<ExprPtr>& values,
+      const std::vector<std::string>& columnNames,
+      const RowTypePtr& outputType,
+      const std::unordered_map<std::string, std::string>& options = {})
+      : LogicalPlanNode(NodeKind::kTableWrite, id, {input}, outputType),
+        connectorId_(connectorId),
+        tableName_(tableName),
+        writeKind_(kind),
+        values_(values),
+        columnNames_(columnNames),
+        options_(options) {}
+
+  const std::string& connectorId() const {
+    return connectorId_;
+  }
+
+  const std::string& tableName() const {
+    return tableName_;
+  }
+
+  WriteKind writeKind() const {
+    return writeKind_;
+  }
+
+  const std::vector<ExprPtr>& values() const {
+    return values_;
+  }
+
+  const std::vector<std::string>& columnNames() const {
+    return columnNames_;
+  }
+
+  const std::unordered_map<std::string, std::string>& options() const {
+    return options_;
+  }
+
+  void accept(const PlanNodeVisitor& visitor, PlanNodeVisitorContext& context)
+      const override;
+
+ private:
+  static RowTypePtr makeWriteType();
+
+  const std::string connectorId_;
+  const std::string tableName_;
+  const WriteKind writeKind_;
+  const std::vector<ExprPtr> values_;
+  const std::vector<std::string> columnNames_;
+  const std::unordered_map<std::string, std::string> options_;
+};
+
+using TableWriteNodePtr = std::shared_ptr<const TableWriteNode>;
+
 } // namespace facebook::axiom::logical_plan
+
+template <>
+struct fmt::formatter<facebook::axiom::logical_plan::WriteKind>
+    : fmt::formatter<string_view> {
+  template <typename FormatContext>
+  auto format(facebook::axiom::logical_plan::WriteKind k, FormatContext& ctx)
+      const {
+    return formatter<string_view>::format(
+        facebook::axiom::logical_plan::WriteKindName::toName(k), ctx);
+  }
+};